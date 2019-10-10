ee4256bc5cc9a72f6ba8064035e21e85b035424e
herozxb <herozxb@gmail.com>
2019-10-10 16:24:35 +0800

    add clipped

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+import math
 import pygame
 import numpy as np
 
@@ -8,12 +9,12 @@ running = 1
 
 
 
-matrix4x4Projection = np.array([[1.0, 0.0, 0.0, 0.0],[0.0, 1.0, 0.0, 0.0],[0.0, 0.0, 1.0, 0.0],[0.0, 0.0, 0.0, 1.0]])
-matrix_rotateZ = np.array([[1.0, 0.0, 0.0, 0.0],[0.0, 1.0, 0.0, 0.0],[0.0, 0.0, 1.0, 0.0],[0.0, 0.0, 0.0, 1.0]])
-matrix_rotateX = np.array([[1.0, 0.0, 0.0, 0.0],[0.0, 1.0, 0.0, 0.0],[0.0, 0.0, 1.0, 0.0],[0.0, 0.0, 0.0, 1.0]])
-matrix_camera = np.array([[1.0, 0.0, 0.0, 0.0],[0.0, 1.0, 0.0, 0.0],[0.0, 0.0, 1.0, 0.0],[0.0, 0.0, 0.0, 1.0]])
-matrix_view = np.array([[1.0, 0.0, 0.0, 0.0],[0.0, 1.0, 0.0, 0.0],[0.0, 0.0, 1.0, 0.0],[0.0, 0.0, 0.0, 1.0]])
-matrix_I = np.array([[1.0, 0.0, 0.0, 0.0],[0.0, 1.0, 0.0, 0.0],[0.0, 0.0, 1.0, 0.0],[0.0, 0.0, 0.0, 1.0]])
+matrix4x4Projection = np.array( [ [1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0] ] )
+matrix_rotateZ      = np.array( [ [1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0] ] )
+matrix_rotateX      = np.array( [ [1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0] ] )
+matrix_camera       = np.array( [ [1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0] ] )
+matrix_view         = np.array( [ [1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0] ] )
+matrix_I            = np.array( [ [1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0] ] )
 
 def multiplayMatrixVector(inputV,matrix4x4):
     outputV = vector3d(0,0,0)
@@ -178,7 +179,7 @@ meshCube2.append(bottomTriangle2)
 
 meshCube = meshCube1 + meshCube2 + meshCube3 + meshCube4 + meshCubetest
 
-import math
+
 #project matrix
 fNear = 0.1
 fFar = 1000.0
@@ -201,16 +202,17 @@ def Sort(sub_list):
     sub_list.sort(key = lambda x: x[0].line1.z + x[0].line2.z + x[0].line3.z, reverse = True)
     return sub_list
 
-def matirx_rotate_Z(theta):
 
-    matrix_rotateZ[0][0] = math.cos( theta )
-    matrix_rotateZ[0][1] = math.sin( theta )
-    matrix_rotateZ[1][0] = -1 * math.sin( theta )
-    matrix_rotateZ[1][1] = math.cos( theta )
-    matrix_rotateZ[2][2] = 1
-    matrix_rotateZ[3][3] = 1
-
-    return matrix_rotateZ
+def matirx_rotate_X(theta):
+    
+    matrix_rotateX[0][0] = 1
+    matrix_rotateX[1][1] = math.cos( theta * 0.5 )
+    matrix_rotateX[1][2] = math.sin( theta * 0.5 )
+    matrix_rotateX[2][1] = -1 * math.sin( theta * 0.5 )
+    matrix_rotateX[2][2] = math.cos( theta * 0.5 )
+    matrix_rotateX[3][3] = 1
+    
+    return matrix_rotateX
 
 def matirx_rotate_Y(theta):
     
@@ -223,16 +225,17 @@ def matirx_rotate_Y(theta):
     
     return matrix_rotateX
 
-def matirx_rotate_X(theta):
-    
-    matrix_rotateX[0][0] = 1
-    matrix_rotateX[1][1] = math.cos( theta * 0.5 )
-    matrix_rotateX[1][2] = math.sin( theta * 0.5 )
-    matrix_rotateX[2][1] = -1 * math.sin( theta * 0.5 )
-    matrix_rotateX[2][2] = math.cos( theta * 0.5 )
-    matrix_rotateX[3][3] = 1
-    
-    return matrix_rotateX
+def matirx_rotate_Z(theta):
+
+    matrix_rotateZ[0][0] = math.cos( theta )
+    matrix_rotateZ[0][1] = math.sin( theta )
+    matrix_rotateZ[1][0] = -1 * math.sin( theta )
+    matrix_rotateZ[1][1] = math.cos( theta )
+    matrix_rotateZ[2][2] = 1
+    matrix_rotateZ[3][3] = 1
+
+    return matrix_rotateZ
+
 
 
 def add_each_vector_by_number(vector,x,y,z):
@@ -287,8 +290,6 @@ def vector_normal_make(normal):
     normal.y /= length
     normal.z /= length
 
-
-    
     return normal
 
 def vector_dotProduct(vector1,vector2):
@@ -366,9 +367,109 @@ def matrix_quike_inverse(m):
     
     return matrix4x4
 
+
+
+def line_intersect_plane( plane_point, plane_normal, line_start, line_end ):
+
+    plane_normal      = vector_normal_make(plane_normal)
+    plane_distance    = -1 *  vector_dotProduct( plane_normal, plane_point )
+    a_distance        = vector_dotProduct( line_start, plane_point )
+    b_distance        = vector_dotProduct( line_end, plane_point )
+    t                 = ( -1 * plane_distance - a_distance ) / ( b_distance - a_distance )
+    line_start_to_end = vector_minus_vector( line_end, line_start ) 
+    line_to_intersect = multiply_each_vector_by_number( line_start_to_end, t, t, t )  
+
+    return vector_add_vector( line_start, line_to_intersect )
+
+
+def calculate_distance_from_point_to_plane( point, plane_normal ):
+
+    plane_normal = vector_normal_make(plane_normal) 
+
+    return plane_normal.x * point.x + plane_normal.y * point.y + plane_normal.z * point.z - vector_dotProduct( plane_normal, point )
+
+
+def triangle_clip_against_plane( plane_point, plane_normal, traingle_put_in ):
+
+    plane_normal      = vector_normal_make(plane_normal)    
+
+    inside_points = []
+    counter_of_inside_points = 0
+    outside_points = []
+    counter_of_outside_points = 0
+
+
+    distance_0 = calculate_distance_from_point_to_plane( traingle_put_in.line1, plane_normal )
+    distance_1 = calculate_distance_from_point_to_plane( traingle_put_in.line2, plane_normal )
+    distance_2 = calculate_distance_from_point_to_plane( traingle_put_in.line3, plane_normal )
+
+    if distance_0 >= 0:
+        inside_points.append(traingle_put_in.line1)
+        counter_of_inside_points = counter_of_inside_points + 1
+    else:
+        outside_points.append(traingle_put_in.line1)
+        counter_of_outside_points = counter_of_outside_points + 1
+
+
+    if distance_1 >= 0:
+        inside_points.append(traingle_put_in.line1)
+        counter_of_inside_points = counter_of_inside_points + 1
+    else:
+        outside_points.append(traingle_put_in.line1)
+        counter_of_outside_points = counter_of_outside_points + 1
+
+
+    if distance_2 >= 0:
+        inside_points.append(traingle_put_in.line1)
+        counter_of_inside_points = counter_of_inside_points + 1
+    else:
+        outside_points.append(traingle_put_in.line1)
+        counter_of_outside_points = counter_of_outside_points + 1
+
+
+    if counter_of_inside_points == 0:
+        return [ 0, None, None] 
+
+    if counter_of_inside_points == 3:
+
+        traingle_put_out_1 = traingle_put_in
+
+        return [ 1, traingle_put_in, None] 
+
+    if counter_of_inside_points == 1 and counter_of_outside_points == 2 :
+
+        traingle_put_out_1.line1 = inside_points[0]
+        traingle_put_out_1.line2 = line_intersect_plane( plane_point, plane_normal, inside_points[0], outside_points[0] ) 
+        traingle_put_out_1.line3 = line_intersect_plane( plane_point, plane_normal, inside_points[0], outside_points[1] ) 
+
+        return [ 1, traingle_put_out_1, None] 
+
+
+    if counter_of_inside_points == 2 and counter_of_outside_points == 1 :
+
+        traingle_put_out_1.line1 = inside_points[0]
+        traingle_put_out_1.line2 = inside_points[1]
+        traingle_put_out_1.line3 = line_intersect_plane( plane_point, plane_normal, inside_points[0], outside_points[0] ) 
+
+        traingle_put_out_2.line1 = inside_points[0]
+        traingle_put_out_2.line2 = traingle_put_out_1.line3
+        traingle_put_out_2.line3 = line_intersect_plane( plane_point, plane_normal, inside_points[1], outside_points[0] ) 
+
+        return [ 2, traingle_put_out_1, traingle_put_out_2 ] 
+
+
+    return true
+
+
+
+
+###################################################### Program Start #########################################################################
+
+
 vectorCamera = vector3d(0.0, 0.0,0.0)
 vectorLookDirection = vector3d(0.0,0.0,1.0)
 fYaw = 0
+
 def Update(elapsedTime):
     
     global vectorLookDirection
@@ -382,33 +483,33 @@ def Update(elapsedTime):
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if(event.key == pygame.K_RIGHT):
-                print("---1---")
+                print("---left---")
                 vectorCamera.x += 8.0 * elapsedTime
             if(event.key == pygame.K_LEFT):
-                print("---2---")
+                print("---right---")
                 vectorCamera.x -= 8.0 * elapsedTime
 
             if(event.key == pygame.K_UP):
-                print("---3---")
+                print("---up---")
                 vectorCamera.y += 8.0 * elapsedTime
             if(event.key == pygame.K_DOWN):
-                print("---4---")
+                print("---down---")
                 vectorCamera.y -= 8.0 * elapsedTime
                     
                     
             if(event.key == pygame.K_a):
-                print("---5---")
+                print("---left turn---")
                 fYaw -= 2.0 * elapsedTime
 
             if(event.key == pygame.K_d):
-                print("---6---")
+                print("---right turn---")
                 fYaw += 2.0 * elapsedTime
 
             if(event.key == pygame.K_w):
-                print("---7---")
+                print("---forward---")
                 vectorCamera = vector_add_vector(vectorCamera,vForward)
             if(event.key == pygame.K_s):
-                print("---8---")
+                print("---backward---")
                 vectorCamera = vector_minus_vector(vectorCamera,vForward)
                     
                     
@@ -432,14 +533,11 @@ def Update(elapsedTime):
         triRotatedZ.line1 = multiplayMatrixVector(triangles.line1,Matirx_Rotate_Z)
         triRotatedZ.line2 = multiplayMatrixVector(triangles.line2,Matirx_Rotate_Z)
         triRotatedZ.line3 = multiplayMatrixVector(triangles.line3,Matirx_Rotate_Z)
-        #print(triRotatedZ.line1.x)
-        
-        
-        #// Rotate in X-Axis
+
+        # Rotate in X-Axis
         triRotatedZX.line1 = multiplayMatrixVector(triRotatedZ.line1,Matirx_Rotate_X)
         triRotatedZX.line2 = multiplayMatrixVector(triRotatedZ.line2,Matirx_Rotate_X)
         triRotatedZX.line3 = multiplayMatrixVector(triRotatedZ.line3,Matirx_Rotate_X)
-        #print(triRotatedZX.line1.x)
         
         # Offset into the screen
         triTranslated = add_each_vector_by_number(triRotatedZX,100,100,100)
@@ -483,14 +581,19 @@ def Update(elapsedTime):
                 lightdirection = vector_normal_make(lightdirection)
 
                 dotProductOflight = vector_dotProduct(normal,lightdirection)
-		#Project triangles from 3D --> 2D
-        
-        
+
+				# Convert World Space      --> View Space
                 triViewed.line1 = multiplayMatrixVector(triTranslated.line1,matrix_view)
                 triViewed.line2 = multiplayMatrixVector(triTranslated.line2,matrix_view)
                 triViewed.line3 = multiplayMatrixVector(triTranslated.line3,matrix_view)
+
+                number_clipped_triangle = 0
+                clipped_triangle = []
+                clip_result = triangle_clip_against_plane( vector3d( 0.0, 0.0, 0.1 ), vector3d( 0.0, 0.0, 1.0 ), triViewed )
+
+
         
-        
+		        #Project triangles from 3D --> 2D        
                 triProjected.line1 = multiplayMatrixVector(triViewed.line1,matrix4x4Projection)
                 triProjected.line2 = multiplayMatrixVector(triViewed.line2,matrix4x4Projection)
                 triProjected.line3 = multiplayMatrixVector(triViewed.line3,matrix4x4Projection)
